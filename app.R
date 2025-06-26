d49c340b8d03265687bf2cfe4778c8b27f2d00cf
svalbuenaa <31705507+svalbuenaa@users.noreply.github.com>
2025-06-26 18:40:35 +0200

    Color genes based on user-defined thresholds

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ dataset <- read.csv(proj_dir)
 significance_level <- 0.05
 
 # Start modifying dataset to prepare it for rendering
-dataset$Gene <- rownames(dataset)
+names(dataset)[names(dataset) == 'X'] <- 'Gene'
 dataset %>% filter(padj<significance_level) %>% arrange(padj)
 
 
@@ -32,7 +32,10 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             textInput("pvalue_user",
-                        "pvalue threshold:")
+                        "pvalue threshold:"),
+            
+            textInput("log2FoldChange_user",
+                      "log2FoldChange threshold:")
         ),
 
         # Show a plot of the generated distribution
@@ -47,15 +50,39 @@ server <- function(input, output) {
 
     output$distPlot <- renderPlot({
         tryCatch({
-          pvalue_ <- as.numeric(input$pvalue_user)
-          if(pvalue_ > 0 & pvalue_ <= 1){
-            ggplot(dataset %>% filter(padj < pvalue_), aes(x=log2FoldChange, y=-log10(padj), label=Gene))+ 
-              geom_point()
+          # Get values provided by user
+          threshold_pvalue_ <- as.numeric(input$pvalue_user)
+          threshold_log2FoldChange_ <- as.numeric(input$log2FoldChange_user)
+          # threshold_pvalue_ <- 0.05
+          # threshold_log2FoldChange_ <- 1
+          # Update dataset significance column based on user-provided values
+          dataset$significance <- NA
+          for (gene in 1:nrow(dataset)){
+             if (dataset[gene,"log2FoldChange"]>=threshold_log2FoldChange_ & dataset[gene,"padj"]< threshold_pvalue_){
+               dataset[gene, "significance"] <- "Up"
+             }
+
+             else if (dataset[gene,"log2FoldChange"]<=((-1)*threshold_log2FoldChange_) & dataset[gene,"padj"]< threshold_pvalue_){
+               dataset[gene, "significance"] <- "Down"
+             }
+
+             else if (abs(dataset[gene,"log2FoldChange"])<threshold_log2FoldChange_ | dataset[gene,"padj"]>= threshold_pvalue_){
+               dataset[gene, "significance"] <- "Not significant"
+             }
+          }
+          
+          if(threshold_pvalue_ > 0 & threshold_pvalue_ <= 1 & threshold_log2FoldChange_ > 0){
+            ggplot(dataset, aes(x=log2FoldChange, y=-log10(padj), label=Gene, color=significance))+ 
+              geom_point(size=0.8)+
+              scale_color_manual(values = c("Up"="#b02428", "Not significant"="grey", "Down"="#6697ea"))+ 
+              labs(title="Plot")
           }
         },
         error = function(e){
           ggplot()+
-            labs(title="Please, insert numeric values between 0 and 1")
+            labs(title="Please, insert valud numeric values:",
+                 subtitle = "--Insert values from 0 to 1 for the pvalue\n--Insert values larger than 0 for log2FoldChange")+
+            theme_void()
         })
         
         # draw the histogram with the specified number of bins
